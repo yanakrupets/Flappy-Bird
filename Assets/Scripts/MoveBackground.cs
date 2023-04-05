b7b7865bf9426bea853455ccb0e1ad5aede6e3f1
Krupets Yana <malenkayakoshechka17@gmail.com>
2023-04-06 01:27:56 +0300

    background movement

---
+++ Assets/Scripts/MoveBackground.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveBackground : MonoBehaviour 
+{
+	[SerializeField] private float _speed;
+	[SerializeField] private float _destinationPosition;
+	[SerializeField] private float _startPosition;
+
+	private float x;
+
+	void Update () 
+	{
+		x = transform.position.x;
+		x += _speed * Time.deltaTime;
+		transform.position = new Vector3 (x, transform.position.y, transform.position.z);
+
+		if (x <= _destinationPosition) 
+		{
+			x = _startPosition;
+			transform.position = new Vector3 (x, transform.position.y, transform.position.z);
+		}
+	}
+}
